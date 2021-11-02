9f55c918f80d279d660d7146f4317e5f51f2286f
Fran Delgado <frandelgado@git.com>
2021-11-02 12:06:45 +0100

    adecuar prints a formato y funciones de logging

--- code_argparse.py
+++ code_argparse.py
@@ -1,29 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import argparse
 import sys
 import settings
+import logging
+import os
 
+py_script= os.path.basename(__file__)
+
+
+settings.init_settings()
+settings.init_config_log()
+
+settings.print_settings()
+
+usuario     = settings.usuario
+path_base   = settings.path_base
+host        = settings.host
+hora_inicio = settings.hora
+
+######################################################################################
+# Definición de niveles de criticidad de logging.
+######################################################################################
+
+def log_d(mensaje): logging.debug(f"{py_script} {mensaje}")
+def log_i(mensaje): logging.info(f"{py_script} {mensaje}")
+def log_w(mensaje): logging.warning(f"{py_script} {mensaje}")
+def log_e(mensaje): logging.error(f"{py_script} {mensaje}")
+def log_c(mensaje): logging.critical(f"{py_script} {mensaje}")
+
+######################################################################################
+# Validación de parámetros de entrada en el caso de que el script los necesite 
+######################################################################################
 NUM_PARAMS_ENTRADA=5
-
 PARAMS_VALIDOS={"Nombre_parm_1 ": ("A","B","C"),          
                 "Nombre_parm_2 ": ("AA","BB","CC"),       
                 "Nombre_parm_3 ": ("AAA","BBB","CCC"),    
                 "Nombre_parm_4 ": ("J","K"),              
                 "Nombre_parm_5 ": ("B")}                  
 
-if len(PARAMS_VALIDOS) != NUM_PARAMS_ENTRADA:
-    print("ERROR. Numéro de parámetros validos inconsistente con posibles valores permitidos")
-    sys.exit(8)
+if NUM_PARAMS_ENTRADA >0 :
+    if (len(PARAMS_VALIDOS) != NUM_PARAMS_ENTRADA):
+      print(f"ERROR. Numéro de parámetros validos inconsistente con posibles valores permitidos")
+      log_e(f"ERROR. Numéro de parámetros validos inconsistente con posibles valores permitidos")
+      sys.exit(8)
 
-######################################################################################
-# Validación de parámetros de entrada en el caso de que el script los necesite 
-#######################################################################################
 # Función para validar que los parámetros están dentro de los valores previstos
 # Atención args.parametros es una lista y evidentemente los elementos empiezan por el orden 0 por eso restamos 1
 def control_parametros(orden_parametro,key_lista_ok,lista_ok,lista_parametros_script):
     if (lista_parametros_script[orden_parametro-1] not in lista_ok):
-            print("ERROR en parámetro : ",key_lista_ok, "--> orden del parámetro =",orden_parametro)  
-            print(" --> Valores posibles:", lista_ok)
-            print("RC = ",orden_parametro)
+            print(f"ERROR en parámetro {key_lista_ok}--> orden del parámetro / RC = {orden_parametro}")  
+            print(f" --> Valores posibles:{lista_ok}")
+            log_e(f"ERROR en parámetro {key_lista_ok}--> orden del parámetro / RC = {orden_parametro}")  
+            log_e(f" --> Valores posibles: {lista_ok}")
             sys.exit(orden_parametro)
                
 
@@ -40,9 +70,9 @@ def validar_parametros_entrada():
  
     # Controlamos que como mínimo hayan 3 parámetros, aunque solo validamos los tres primeros. 
     if len(args.parametros) < NUM_PARAMS_ENTRADA: 
-        print("Error : Mínimo se han de informar",NUM_PARAMS_ENTRADA,"parámetros")
         retcode = 66 
-        print("RC = ",retcode)
+        print(f"Error : Mínimo se han de informar{NUM_PARAMS_ENTRADA}parámetros. RC = {retcode}")
+        log_e(f"Error : Mínimo se han de informar{NUM_PARAMS_ENTRADA}parámetros. RC = {retcode}")
         sys.exit(retcode)
     else: 
         for parametro in args.parametros:
@@ -64,26 +94,17 @@ def validar_parametros_entrada():
 ######################################################################################
 # PROGRAMA PRINCIPAL 
 #######################################################################################
-# Recuperar settings.
-settings.init_settings()
-settings.print_settings()
 
-usuario     = settings.usuario
-path_base   = settings.path_base
-host        = settings.host
-hora_inicio = settings.hora
 
-# Validar parámetros de entrada al script.
+########## Validar parámetros de entrada al script.
 if NUM_PARAMS_ENTRADA == 0:
-    print("AVISO : Script sin parámetros de entrada")
+    log_d("AVISO : Script sin parámetros de entrada")
     pass
 else: 
     parms_val_ok,resto_parms=validar_parametros_entrada()
-    print("Validación correcta de parámetros de entrada")
-    print("- Parámetros validados = ",parms_val_ok)
-    print("- Parámetros no validados = ",resto_parms)
-
-
+    log_d(" Validación correcta de parámetros de entrada")
+    log_d("- Parámetros validados = "+str(parms_val_ok))
+    log_d("- Parámetros no validados = "+str(resto_parms))
 
 
      
--- settings.py
+++ settings.py
@@ -3,6 +3,15 @@ import os
 import getpass
 import datetime
 import socket
+import logging
+
+py_script = os.path.basename(__file__)
+
+def init_config_log():
+    logging.basicConfig(filename="fichero_de_logging.log", \
+                    filemode='w', \
+                    level=logging.DEBUG, \
+                    format=' %(asctime)s -  %(levelname)s - %(message)s')
 
 def init_settings():
     global usuario,    \
@@ -25,3 +34,12 @@ def print_settings():
     print("--> Hora inicio :      ",hora)
     print("--> Script settings :  ",script)
     print('----------------------------------------------------------------------------------------------------------')
+    
+    logging.debug('----------------------------------------------------------------------------------------------------------')
+    logging.debug(' Variables de entorno : ')
+    logging.debug("--> Host :             "+ host)
+    logging.debug("--> Usuario :          "+ usuario)
+    logging.debug("--> Directorio :       "+ path_base)
+    logging.debug("--> Hora inicio :      "+ str(hora))
+    logging.debug("--> Script settings :  "+ script)
+    logging.debug('----------------------------------------------------------------------------------------------------------')
